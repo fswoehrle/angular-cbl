06bc73cb9344f3fa4fd44181b5893690d9b4e020
fsw <fsw1@gmx.de>
2021-01-08 22:09:46 +0100

    bigfix removed favicon

--- src/app/book-contents/book-contents.component.ts
+++ src/app/book-contents/book-contents.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-//import { LessonService } from "../lesson.service";
-import { Observable } from 'rxjs';
 
 import { CblContent } from '../core/model/cblContent.model';
 import { ContentService } from '../content.service';
@@ -12,30 +10,20 @@ import { ContentService } from '../content.service';
   styleUrls: ["./book-contents.component.css"]
 })
 export class BookContentsComponent implements OnInit {
-  //content: Observable<CblContent[]>;
   loading: Boolean = false;
   errorMessage: String = '';
-  //a: CblContent;
-  //b: CblContent;
   contentArray: CblContent[] = [];
-  //content:CblContent;
 
   constructor(
-    private route: ActivatedRoute, 
+    private route: ActivatedRoute,
     private contentService: ContentService
-    //private lessonService: LessonService
-    ) {}
+  ) { }
 
   ngOnInit() {
     this.loading = true;
 
     // First get the book id from the current route.
     const bookId = this.route.snapshot.paramMap.get("bookId");
-    //console.log("bookid: " + bookId)
-
-    //this.loading = true;
-
-    //this.content = this.contentService.list(bookId);
 
     this.contentService.list(bookId)
       .subscribe(
@@ -48,25 +36,5 @@ export class BookContentsComponent implements OnInit {
           this.loading = false;
         }
       )
-
-      console.log("contentArray: " + this.contentArray[0]);
-    
-    //this.loading = false;
-    
-
-/*    
-    this.lessonService.getContents(bookId)
-      .subscribe(
-        (response) => {
-          this.content = response;
-          this.loading = false;
-        },
-        (error) => {
-          this.errorMessage = error;
-          this.loading = false;
-        }
-      )
-      */
   }
-
 }
